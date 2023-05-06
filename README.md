51f627329b2a1807b01b7f6e6bc019dffefc75ec
CacaAlves00 <cacazinho99@gmail.com>
2023-05-05 21:44:30 -0300

    Initial setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# travel-planning
+A travel-planning app.
+
+[Project design in Figma](https://www.figma.com/file/LVZ2NWQhBk3ctXonYirSA8/Untitled?type=design&node-id=0-1)
+
---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World !')
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log('Server started on port 3000')
+})
