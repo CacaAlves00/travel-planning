82da09169e1c5d6668ed565346835bbf2af66563
CacaAlves00 <cacazinho99@gmail.com>
2023-05-07 02:34:46 -0300

    Styling AddButton

---
+++ frontend/src/components/AddButton/AddButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './AddButton.scss'
+
+function AddButton() {
+  return (
+    <div className='add-button scale-on-hover'>+</div>
+  )
+}
+
+export default AddButton
